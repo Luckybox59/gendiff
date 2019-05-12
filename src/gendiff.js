5fa7967fa71153eaeb000c4281ec5e680dc9c5e2
Luckybox59 <Luckybox59@gmail.com>
2019-05-12 19:48:35 +0500

    deleted unnecessary data from the tree

--- src/gendiff.js
+++ src/gendiff.js
@@ -7,9 +7,7 @@ import render from './formatters';
 const propertyActions = [
   {
     check: (data1, data2, key) => !has(data1, key) && has(data2, key),
-    action: (gap, path, key, data1, data2) => ({
-      gap,
-      path,
+    action: (key, data1, data2) => ({
       key,
       type: 'added',
       value: data2[key],
@@ -17,9 +15,7 @@ const propertyActions = [
   },
   {
     check: (data1, data2, key) => has(data1, key) && !has(data2, key),
-    action: (gap, path, key, data1) => ({
-      gap,
-      path,
+    action: (key, data1) => ({
       key,
       type: 'deleted',
       value: data1[key],
@@ -28,9 +24,7 @@ const propertyActions = [
   {
     check: (data1, data2, key) => has(data1, key) && has(data2, key)
       && data1[key] === data2[key],
-    action: (gap, path, key, data1) => ({
-      gap,
-      path,
+    action: (key, data1) => ({
       key,
       type: 'saved',
       value: data1[key],
@@ -39,20 +33,16 @@ const propertyActions = [
   {
     check: (data1, data2, key) => has(data1, key) && has(data2, key)
       && isObject(data1[key]) && isObject(data2[key]),
-    action: (gap, path, key, data1, data2, fn) => ({
-      gap,
-      path,
+    action: (key, data1, data2, fn) => ({
       key,
       type: 'nested',
-      children: fn(data1[key], data2[key], [...path, key], gap + 4),
+      children: fn(data1[key], data2[key]),
     }),
   },
   {
     check: (data1, data2, key) => has(data1, key) && has(data2, key)
       && data1[key] !== data2[key],
-    action: (gap, path, key, data1, data2) => ({
-      gap,
-      path,
+    action: (key, data1, data2) => ({
       key,
       type: 'updated',
       oldValue: data1[key],
@@ -64,11 +54,11 @@ const propertyActions = [
 const getProperty = (data1, data2, key) => propertyActions
   .find(({ check }) => check(data1, data2, key));
 
-const makeAst = (data1, data2, path = [], gap = 2) => {
+const makeAst = (data1, data2) => {
   const keys = union(Object.keys(data1), Object.keys(data2)).sort();
   const ast = keys.map((key) => {
     const options = getProperty(data1, data2, key)
-      .action(gap, path, key, data1, data2, makeAst);
+      .action(key, data1, data2, makeAst);
     return options;
   });
   return ast;
